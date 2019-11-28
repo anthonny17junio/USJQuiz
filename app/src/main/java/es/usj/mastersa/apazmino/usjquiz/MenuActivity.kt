77e4fc9ec09a1493e493c048531801c20f721239
Anthonny Pazmiño <anthonny17junio@gmail.com>
2019-11-28 14:46:11 +0100

    Creación de menú activity

---
+++ app/src/main/java/es/usj/mastersa/apazmino/usjquiz/MenuActivity.kt
@@ -0,0 +1,12 @@
+package es.usj.mastersa.apazmino.usjquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
