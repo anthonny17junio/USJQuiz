085d8f6fd360a14c71781d43e55a3afa093bf282
AnderSU30 <ander@soroa.org>
2019-12-30 16:23:54 +0100

    Conseguido: - Live location
    No conseguido:
    - Add KML layer

--- app/src/main/java/es/usj/mastersa/apazmino/usjquiz/ActivityGPS.kt
+++ app/src/main/java/es/usj/mastersa/apazmino/usjquiz/ActivityGPS.kt
@@ -1,19 +1,149 @@
 package es.usj.mastersa.apazmino.usjquiz
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_gps.*
 
-class ActivityGPS : AppCompatActivity() {
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+
+
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.data.kml.KmlLayer
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.PermissionListener
+
+
+import org.xmlpull.v1.XmlPullParserException
+
+
+class ActivityGPS : AppCompatActivity(), OnMapReadyCallback, PermissionListener {
+
+    companion object {
+        const val REQUEST_CHECK_SETTINGS = 43
+    }
+
+
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gps)
-        btnBack.setOnClickListener{ back()}
+        //setSupportActionBar(toolbar)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        //To fetch the user´s current location or last known location
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        addLayer()
     }
-    fun back(){
-        val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
+
+    fun addLayer(){
+        val layer = KmlLayer(mMap, R.raw.etxe_project, applicationContext)
+        layer.addLayerToMap()
+
     }
+
+
+    //To make the activity know, when the map is ready
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap?: return
+        if (isPermissionGiven()) {
+            googleMap.isMyLocationEnabled = true
+            googleMap.uiSettings.isMyLocationButtonEnabled = true
+            googleMap.uiSettings.isZoomControlsEnabled = true
+            getCurrentLocation()
+        } else {
+            givePermission()
+        }
+
+        // Add a marker in Sydney and move the camera
+        /*val usj = LatLng(41.756747, -0.832058)
+        mMap.addMarker(MarkerOptions().position(usj).title("Marker in USJ"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(usj))*/
+
+    }
+
+    private fun isPermissionGiven(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun givePermission() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(this).check()
+    }
+
+    private fun getCurrentLocation() {
+        val locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = (10*1000).toLong()
+        locationRequest.fastestInterval = 2000
+
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(locationRequest)
+        val locationSettingsRequest = builder.build()
+
+        val result = LocationServices.getSettingsClient(this).checkLocationSettings(locationSettingsRequest)
+        result.addOnCompleteListener {task ->
+            try {
+                val response = task.getResult(ApiException::class.java)
+                //if (response!!.locationSettingsStates.isLocationPresent) {
+                //}
+            }catch (exception: ApiException) {
+                LocationSettingsStatusCodes.RESOLUTION_REQUIRED
+                try {
+                    val resolvable = exception as ResolvableApiException
+                    resolvable.startResolutionForResult(this, REQUEST_CHECK_SETTINGS)
+                } catch (e: IntentSender.SendIntentException) {
+                } catch (e: ClassCastException){
+                }
+                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode) {
+            REQUEST_CHECK_SETTINGS -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    getCurrentLocation()
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+
+    }
+
+    override fun onPermissionRationaleShouldBeShown(
+        permission: com.karumi.dexter.listener.PermissionRequest?,
+        token: PermissionToken?
+    ) {
+    }
+    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+    }
+
+
 }
+
